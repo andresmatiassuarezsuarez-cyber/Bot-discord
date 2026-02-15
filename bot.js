6b0d43f7c3c88470669a96e6092335bb16c30747
andresmatiassuarezsuarez-cyber <andresmatiassuarezsuarez@gmail.com>
2026-02-15 20:03:09 +0100

    Remove import of keepAlive.js from bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import './keepAlive.js';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
